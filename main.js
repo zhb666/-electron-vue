f829f3318ef2829bc11e992c98a375f2981c24c3
“tes <462612421@qq.com>
2019-06-13 14:06:31 +0800

    打包

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('dist/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
